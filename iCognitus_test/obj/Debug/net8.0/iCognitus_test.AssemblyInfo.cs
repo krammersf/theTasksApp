35a858d259f4e6dda6c0685ad7c1f6465390e709
Frederico Bajouco <frederico.bajouco@gmail.com>
2025-01-20 09:26:25 +0000

    Update AssemblyInfo with new informational version and cache inputs

--- iCognitus_test/obj/Debug/net8.0/iCognitus_test.AssemblyInfo.cs
+++ iCognitus_test/obj/Debug/net8.0/iCognitus_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iCognitus_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504191e03c9715f6e257f205d61f527b5e3de783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58cf6d297eb4eaca0b1ff791e149330c175157b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("iCognitus_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iCognitus_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
