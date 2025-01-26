6ee4483abab6db6764c7e3a4aac763e5e0bed4fd
Frederico Bajouco <frederico.bajouco@gmail.com>
2025-01-26 18:06:29 +0000

    Add scroll to top functionality on task detail view; format run.py for consistency

--- Frontend-theTasksApp/src/app/Components/task-detail/task-detail.component.ts
+++ Frontend-theTasksApp/src/app/Components/task-detail/task-detail.component.ts
@@ -18,6 +18,7 @@ export class TaskDetailComponent implements OnInit {
 	constructor(private route: ActivatedRoute, private taskService: TaskService, private router: Router) {}
 
 	ngOnInit(): void {
+		window.scrollTo(0, 0);
 		const taskId = Number(this.route.snapshot.paramMap.get('id'));
 		this.taskService.getTaskById(taskId).subscribe(task => {
 			this.task = task;
